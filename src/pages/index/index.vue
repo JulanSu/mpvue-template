9e649a2e6327d23043263fdcb0264e5afa5100a8
guoyaning <jlgyn1@163.com>
2019-07-05 14:40:04 +0800

    init template

---
+++ src/pages/index/index.vue
@@ -0,0 +1,43 @@
+<template>
+	<div @click="clickHandle">
+		<p class="title">
+			我是第一个
+			<span>我是第二个</span>
+		</p>
+	</div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      name: 'mpvue'
+    }
+  },
+
+  methods: {
+
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.userinfo {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+p {
+  width: 40px;
+  height: 30px;
+  span {
+    color: #f00;
+  }
+}
+
+</style>
